c115e5707d77ae01365dff984cc1fed3552f770d
multipletwigs <starlaxone@gmail.com>
2022-01-05 12:11:42 +0800

    Added more init files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div
+    class="flex flex-col justify-center items-center gap-10 w-screen h-screen bg-gradient-to-br from-violetblue via-lightblue to-turquoise"
+  >
+    <img
+      class="sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 w-1/2 h-auto"
+      alt="Eureka logo"
+      src="../assets/logo.png"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloEureka from "../components/HelloEureka.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    HelloEureka,
   },
 })
 export default class Home extends Vue {}
